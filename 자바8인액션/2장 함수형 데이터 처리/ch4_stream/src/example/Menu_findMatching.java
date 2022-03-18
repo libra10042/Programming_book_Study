b0e42aca34b0ddb05289f64035cc4f714baea9fc
libra10042 <libra10042@gmail.com>
2022-03-18 17:05:26 +0900

    ch5 stream : search and matching ( allMatch, anyMatch, noneMatch, findFirst, findAny )

--- 자바8인액션/2장 함수형 데이터 처리/ch4_stream/src/example/Menu_findMatching.java
+++ 자바8인액션/2장 함수형 데이터 처리/ch4_stream/src/example/Menu_findMatching.java
@@ -2,6 +2,7 @@ package example;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Menu_findMatching {
 
@@ -24,6 +25,40 @@ public class Menu_findMatching {
             System.out.println("The Menu is (somewhat) vegetarian friendly!!");
         }
 
+        // 프레디케이트가 모든 요소와 일치하는지 검사
+        boolean isHealthy = menu3.stream()
+                .allMatch(d -> d.getCalories() < 1000);
+        System.out.println(isHealthy);
+
+
+        // noneMatch
+        boolean isHealthy2 = menu3.stream()
+                .noneMatch(d -> d.getCalories() >= 1000);
+
+        System.out.println(isHealthy2);
+
+
+        // findAny : 요소 검색.
+        Optional<Dish> dish =
+                menu3.stream()
+                        .filter(Dish::isVegetarian)
+                        .findAny();
+
+        menu3.stream()
+                .filter(Dish::isVegetarian)
+                .findAny()
+                .ifPresent(d -> System.out.println(d.getName()));
+
+
+        // 첫번째 요소 찾기. : 숫자 리스트에서 3으로 나누어떨어지는 첫 번째 제곱값을 반환하는 예제.
+        List<Integer> someNumbers = Arrays.asList(1,2,3,4,5);
+        Optional<Integer> firstSquareDivisibleByThree =
+                someNumbers.stream()
+                        .map(x -> x * x)
+                        .filter(x -> x % 3 == 0)
+                        .findFirst(); // 9
 
     }
+
+
 }
