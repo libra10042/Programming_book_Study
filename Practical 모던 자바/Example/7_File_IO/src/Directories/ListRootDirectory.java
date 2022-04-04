45b80f1d9571d0abc8f902cd442dff75a9214848
libra10042 <libra10042@gmail.com>
2022-04-04 22:22:10 +0900

    루트 디렉터리

---
+++ Practical 모던 자바/Example/7_File_IO/src/Directories/ListRootDirectory.java
@@ -0,0 +1,20 @@
+package Directories;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+/*
+* 파일 NIO 를 이용한 루트 디렉터리 조회
+* */
+public class ListRootDirectory {
+
+    public static void main(String[] args) {
+        // 루트 디렉터리 조회
+        Iterable<Path> rootDirs = FileSystems.getDefault().getRootDirectories();
+
+        for(Path rootPath : rootDirs){
+            System.out.println(rootPath);
+        }
+    }
+
+}
