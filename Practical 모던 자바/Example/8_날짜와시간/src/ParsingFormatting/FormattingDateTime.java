fc514a9042928f3b35fa871bf720b2e591a2afbd
libra10042 <libra10042@gmail.com>
2022-04-11 21:17:36 +0900

    parsing and formatting

---
+++ Practical 모던 자바/Example/8_날짜와시간/src/ParsingFormatting/FormattingDateTime.java
@@ -0,0 +1,26 @@
+package ParsingFormatting;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/*
+* 날짜와 시간 객체를 DateTimeFormatter 를 이용해서 문자열로 변환하는 예제
+* */
+public class FormattingDateTime {
+
+    public static void main(String[] args) {
+        try{
+            // 현재 날짜와 시간을 구한다.
+            LocalDateTime dateTime = LocalDateTime.now();
+
+            // 사용자 정의된 포맷을 정의한다.
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formatedDate = dateTime.format(formatter);
+            System.out.printf("%s%n", formatedDate);
+        }catch(DateTimeParseException e){
+            e.printStackTrace();
+        }
+    }
+
+}
